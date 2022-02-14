8d77217d043bf2a7640a8f57df9cf3d6908f7365
Bryambepz <bryamparra2709@gmail.com>
2022-02-14 17:49:01 -0500

    Creacion services y funcionamiento paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConversionMonedaComponent } from './pages/conversion-moneda/conversion-moneda.component';
 import { EstadoCuentaComponent } from './pages/estado-cuenta/estado-cuenta.component';
 import { SimuladorAhorrosComponent } from './pages/simulador-ahorros/simulador-ahorros.component';
+import { TransferenciasComponent } from './pages/transferencias/transferencias.component';
 
 const routes: Routes = [
   {path: "conversionMoneda", component: ConversionMonedaComponent},
   {path: "estadoCuenta", component: EstadoCuentaComponent},
-  {path: "simuladorAhorros", component: SimuladorAhorrosComponent}
+  {path: "simuladorAhorros", component: SimuladorAhorrosComponent},
+  {path: "transferencia", component: TransferenciasComponent}
 ];
 
 @NgModule({
--- src/app/pages/transferencias/transferencias.component.ts
+++ src/app/pages/transferencias/transferencias.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { personaS } from 'src/app/domain/persona';
+import { TransferenciaswsService } from 'src/app/services/transferenciasws.service';
 
 @Component({
   selector: 'app-transferencias',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transferencias.component.css']
 })
 export class TransferenciasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  monto: number = 0;
+  saldo: number = 0;
+  personaOb: personaS = new personaS();
+  personaBc: personaS = new personaS();
+  cedulaReceptor: String = "";
+  constructor(private transferencias: TransferenciaswsService) { 
+    
   }
 
+  ngOnInit(): void {
+    this.cargarDatosS();   
+    this.cargarSaldo(); 
+  }
+
+  cargarDatosS(){
+    this.transferencias.getUserSesion().subscribe((data:any) => {
+      console.log(data);
+      this.personaOb = data;
+      
+    });
+  }
+
+  cargarSaldo(){
+    return this.transferencias.getUserSesionSaldo().subscribe((data:any) => {
+      console.log("saldo en cuenta "+data)
+      this.saldo = data;      
+    });
+  }
+
+  buscarCedula(){
+    this.transferencias.getReceptorCed(this.personaBc).subscribe((data:any) => {
+      console.log(data);
+      this.personaBc = data;
+    });
+  }
+
+  realizarT(){
+    this.transferencias.transferir(this.personaBc).subscribe((data:any) => {
+      console.log(data);
+    })
+  }
 }

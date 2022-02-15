21c7764b8018bef46a8e7bb66514e0996c5640fe
Bryambepz <bryamparra2709@gmail.com>
2022-02-15 03:14:47 -0500

    Finalizacion de servicio rest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministracionDeudasComponent } from './pages/administracion-deudas/administracion-deudas.component';
+import { AprovacionPrestamoComponent } from './pages/aprovacion-prestamo/aprovacion-prestamo.component';
 import { ConversionMonedaComponent } from './pages/conversion-moneda/conversion-moneda.component';
 import { EstadoCuentaComponent } from './pages/estado-cuenta/estado-cuenta.component';
 import { SimuladorAhorrosComponent } from './pages/simulador-ahorros/simulador-ahorros.component';
@@ -9,7 +11,9 @@ const routes: Routes = [
   {path: "conversionMoneda", component: ConversionMonedaComponent},
   {path: "estadoCuenta", component: EstadoCuentaComponent},
   {path: "simuladorAhorros", component: SimuladorAhorrosComponent},
-  {path: "transferencia", component: TransferenciasComponent}
+  {path: "transferencia", component: TransferenciasComponent},
+  {path: "pagarDeuda", component: AdministracionDeudasComponent},
+  {path: "aprovacion", component: AprovacionPrestamoComponent}
 ];
 
 @NgModule({
